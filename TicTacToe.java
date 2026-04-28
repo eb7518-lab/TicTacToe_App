8c1cd049ec16ee8e1888b8e423952377f0d46fa8
eb7518-lab <eb7518@srmist.edu.in>
2026-04-28 15:25:58 +0530

    Coded UC8:Continuous Turn-Based Game Loop

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,32 +1,26 @@
-import java.util.Random;
-
 public class TicTacToe {
 
-    static char[][] board = {
-        {'-', '-', '-'},
-        {'-', '-', '-'},
-        {'-', '-', '-'}
-    };
-
-    static char computerSymbol = 'O';
+    static boolean isHumanTurn = true;
+    static boolean gameOver = false;
 
     public static void main(String[] args) {
-        computerMove();
-    }
 
-    static void computerMove() {
-        Random random = new Random();
+        int turns = 0;
 
-        while (true) {
-            int slot = random.nextInt(9) + 1;
+        while (!gameOver) {
 
-            int row = (slot - 1) / 3;
-            int col = (slot - 1) % 3;
+            if (isHumanTurn) {
+                System.out.println("Human Turn");
+            } else {
+                System.out.println("Computer Turn");
+            }
 
-            if (board[row][col] == '-') {
-                board[row][col] = computerSymbol;
-                System.out.println("Computer placed at row " + row + ", col " + col);
-                break;
+            turns++;
+
+            if (turns == 5) {
+                gameOver = true;
+            } else {
+                isHumanTurn = !isHumanTurn;
             }
         }
     }
